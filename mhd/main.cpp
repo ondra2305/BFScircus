6d15e9663918a44789b6d7501d00be46aada33da
Ondřej Svoboda <ondra2305@gmail.com>
2023-06-27 16:52:27 +0200

    MHD: pridane slozitejsi asserty

--- mhd/main.cpp
+++ mhd/main.cpp
@@ -25,154 +25,239 @@ using namespace std;
 class CPublicTransport
 {
 public:
-	CPublicTransport()
-	{
-		m_LineCounter = 0;
-	}
+    CPublicTransport()
+    {
+        m_LineCounter = 0;
+    }
 
-	CPublicTransport &addLine(unsigned cost, const vector<string> &stops)
-	{
-		m_LineCosts[m_LineCounter] = cost;
+    CPublicTransport &addLine(unsigned cost, const vector<string> &stops)
+    {
+        m_LineCosts[m_LineCounter] = cost;
 
-		for (auto &stop : stops)
-		{
-			for (auto &stop1 : stops)
-			{
-				if (stop == stop1)
-					continue;
-				m_Graph[stop].insert({stop1, m_LineCounter});
-			}
-		}
+        for (auto &stop : stops)
+        {
+            for (auto &stop1 : stops)
+            {
+                if (stop == stop1)
+                    continue;
+                m_Graph[stop].insert({stop1, m_LineCounter});
+            }
+        }
 
-		m_LineCounter++;
-		return *this;
-	}
+        m_LineCounter++;
+        return *this;
+    }
 
-	CPublicTransport &optimize()
-	{
-		// NOIDONTTHINKSO
-		return *this;
-	}
+    CPublicTransport &optimize()
+    {
+        // NOIDONTTHINKSO
+        return *this;
+    }
 
-	unsigned findCheapest(const string &from, const string &to)
-	{
-		map<string, unsigned> visited;
-		queue<tuple<string, unsigned, size_t>> toVisit;
-		toVisit.emplace(from, 0, -1);
-		visited.insert({from, 0});
+    unsigned findCheapest(const string &from, const string &to)
+    {
+        map<string, unsigned> visited;
+        queue<tuple<string, unsigned, size_t>> toVisit;
+        toVisit.emplace(from, 0, -1);
+        visited.insert({from, 0});
 
-		while (!toVisit.empty())
-		{
-			auto [currentStop, currentCost, currentLine] = toVisit.front();
-			toVisit.pop();
+        while (!toVisit.empty())
+        {
+            auto [currentStop, currentCost, currentLine] = toVisit.front();
+            toVisit.pop();
 
-			for (auto &neighbor : m_Graph.at(currentStop))
-			{
-				unsigned newCost = currentCost;
-				if (neighbor.second != currentLine)
-				{ // if we are changing line, we pay
-					newCost += m_LineCosts[neighbor.second];
-				}
+            for (auto &neighbor : m_Graph.at(currentStop))
+            {
+                unsigned newCost = currentCost;
+                if (neighbor.second != currentLine)
+                { // if we are changing line, we pay
+                    newCost += m_LineCosts[neighbor.second];
+                }
 
-				if (visited.count(neighbor.first) == 0 || visited[neighbor.first] > newCost)
-				{
-					toVisit.emplace(neighbor.first, newCost, neighbor.second);
-					visited[neighbor.first] = newCost;
-				}
-			}
-		}
+                if (visited.count(neighbor.first) == 0 || visited[neighbor.first] > newCost)
+                {
+                    toVisit.emplace(neighbor.first, newCost, neighbor.second);
+                    visited[neighbor.first] = newCost;
+                }
+            }
+        }
 
-		if (visited.count(to) == 0)
-		{
-			throw logic_error("Connection not found!");
-		}
-		return visited[to];
-	}
+        if (visited.count(to) == 0)
+        {
+            throw logic_error("Connection not found!");
+        }
+        return visited[to];
+    }
 
 private:
-	size_t m_LineCounter;
-	map<string, set<pair<string, unsigned>>> m_Graph;
-	map<size_t, unsigned> m_LineCosts;
+    size_t m_LineCounter;
+    map<string, set<pair<string, unsigned>>> m_Graph;
+    map<size_t, unsigned> m_LineCosts;
 };
 
 int main()
 {
-	CPublicTransport t0;
+    CPublicTransport t;
 
-	t0.addLine(1, {"Staromestska", "Muzeum", "Mustek", "Namesti Miru", "Jiriho z Podebrad"}).addLine(3, {"Hlavni Nadrazi", "Orionka", "Baterie", "Petriny", "Jiriho z Podebrad"}).addLine(2, {"Kamenicka", "Letenske namesti", "Baterie", "Petriny", "Namesti Miru", "Malovanka"}).optimize();
+    t.addLine(3, {"Hlavni Nadrazi", "Orionka", "Baterie", "Petriny", "Jiriho z Podebrad"})
+        .addLine(2, {"Kamenicka", "Letenske namesti", "Baterie", "Petriny", "Namesti Miru", "Malovanka"})
+        .addLine(5, {"Dejvicka", "Muzeum"})
+        .addLine(1, {"Staromestska", "Muzeum", "Mustek", "Namesti Miru", "Jiriho z Podebrad"})
+        .optimize();
 
-	assert(t0.findCheapest("Staromestska", "Baterie") == 3);
-	assert(t0.findCheapest("Staromestska", "Staromestska") == 0);
-	assert(t0.findCheapest("Staromestska", "Namesti Miru") == 1);
-	assert(t0.findCheapest("Staromestska", "Hlavni Nadrazi") == 4);
-	assert(t0.findCheapest("Orionka", "Kamenicka") == 5);
+    assert(t.findCheapest("Staromestska", "Baterie") == 3);
+    assert(t.findCheapest("Staromestska", "Staromestska") == 0);
+    assert(t.findCheapest("Staromestska", "Namesti Miru") == 1);
+    assert(t.findCheapest("Staromestska", "Hlavni Nadrazi") == 4);
+    assert(t.findCheapest("Orionka", "Kamenicka") == 5);
+    assert(t.findCheapest("Jiriho z Podebrad", "Namesti Miru") == 1);
+    assert(t.findCheapest("Dejvicka", "Letenske namesti") == 8);
 
-	t0.addLine(6, {"Vitezne namesti", "I P Pavlova", "Andel"});
+    t.addLine(6, {"Vitezne namesti", "I P Pavlova", "Andel"});
+    t.addLine(9, {"Letenske namesti", "Vitezne namesti"});
 
-	try
-	{
-		t0.findCheapest("Orionka", "Andel");
-	}
-	catch (const logic_error &e)
-	{
-		assert(e.what() != "");
-	}
+    assert(t.findCheapest("Dejvicka", "Andel") == 23);
+    assert(t.findCheapest("Andel", "Dejvicka") == 23);
+    assert(t.findCheapest("Vitezne namesti", "Letenske namesti") == 9);
+    assert(t.findCheapest("Orionka", "Andel") == 20);
 
-	CPublicTransport t;
+    t.addLine(9, {"Terminal 1", "Terminal 2", "Terminal 3", "Nadrazi Veleslavin"});
 
-	t.addLine(3, {"Hlavni Nadrazi", "Orionka", "Baterie", "Petriny", "Jiriho z Podebrad"}).addLine(2, {"Kamenicka", "Letenske namesti", "Baterie", "Petriny", "Namesti Miru", "Malovanka"}).addLine(5, {"Dejvicka", "Muzeum"}).addLine(1, {"Staromestska", "Muzeum", "Mustek", "Namesti Miru", "Jiriho z Podebrad"}).optimize();
+    try
+    {
+        t.findCheapest("Namesti Miru", "Terminal 2");
+    }
+    catch (const logic_error &e)
+    {
+        // assert(e.what() != "");
+        cout << "chyceno" << endl;
+    }
 
-	assert(t.findCheapest("Staromestska", "Baterie") == 3);
-	assert(t.findCheapest("Staromestska", "Staromestska") == 0);
-	assert(t.findCheapest("Staromestska", "Namesti Miru") == 1);
-	assert(t.findCheapest("Staromestska", "Hlavni Nadrazi") == 4);
-	assert(t.findCheapest("Orionka", "Kamenicka") == 5);
-	assert(t.findCheapest("Jiriho z Podebrad", "Namesti Miru") == 1);
-	assert(t.findCheapest("Dejvicka", "Letenske namesti") == 8);
+    CPublicTransport t2;
+    t2.addLine(2, {"A", "B", "C"})
+        .addLine(2, {"C", "D", "E"})
+        .addLine(2, {"E", "F", "G"})
+        .addLine(2, {"G", "H", "I"})
+        .addLine(2, {"I", "J", "K"})
+        .addLine(2, {"K", "L", "M"})
+        .optimize();
 
-	t.addLine(6, {"Vitezne namesti", "I P Pavlova", "Andel"});
-	t.addLine(9, {"Letenske namesti", "Vitezne namesti"});
+    assert(t2.findCheapest("A", "M") == 12);
+    assert(t2.findCheapest("A", "G") == 6);
+    assert(t2.findCheapest("A", "H") == 8);
+    assert(t2.findCheapest("C", "K") == 8);
+    assert(t2.findCheapest("B", "L") == 12);
+    assert(t2.findCheapest("I", "C") == 6);
 
-	assert(t.findCheapest("Dejvicka", "Andel") == 23);
-	assert(t.findCheapest("Andel", "Dejvicka") == 23);
-	assert(t.findCheapest("Vitezne namesti", "Letenske namesti") == 9);
-	assert(t.findCheapest("Orionka", "Andel") == 20);
+    t2.addLine(3, {"A", "Z", "M"}).optimize();
 
-	t.addLine(9, {"Terminal 1", "Terminal 2", "Terminal 3", "Nadrazi Veleslavin"});
+    assert(t2.findCheapest("A", "M") == 3);
+    assert(t2.findCheapest("C", "K") == 7);
+    assert(t2.findCheapest("B", "L") == 7);
 
-	try
-	{
-		t.findCheapest("Namesti Miru", "Terminal 2");
-	}
-	catch (const logic_error &e)
-	{
-		// assert(e.what() != "");
-		cout << "chyceno" << endl;
-	}
+    CPublicTransport t3;
 
-	CPublicTransport t1;
-	t1.addLine(2, {"A", "B", "C"})
-			.addLine(2, {"C", "D", "E"})
-			.addLine(2, {"E", "F", "G"})
-			.addLine(2, {"G", "H", "I"})
-			.addLine(2, {"I", "J", "K"})
-			.addLine(2, {"K", "L", "M"})
-			.optimize();
+    t3.addLine(1, {"A", "B", "C", "D", "E"})
+    .addLine(2, {"F", "G", "H", "E", "I"})
+    .addLine(3, {"J", "K", "L", "B", "M"})
+    .addLine(2, {"N", "O", "P", "Q", "E"})
+    .addLine(4, {"E", "R", "S", "T", "U"})
+    .addLine(5, {"V", "W", "X", "Y", "Z"})
+    .addLine(1, {"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"})
+    .addLine(3, {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M"})
+    .addLine(4, {"V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"})
+    .addLine(2, {"Ac", "Gaa", "Hd", "Ee", "Ie"})
+    .addLine(6, {"E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"});
 
-	assert(t1.findCheapest("A", "M") == 12);
-	assert(t1.findCheapest("A", "G") == 6);
-	assert(t1.findCheapest("A", "H") == 8);
-	assert(t1.findCheapest("C", "K") == 8);
-	assert(t1.findCheapest("B", "L") == 12);
-	assert(t1.findCheapest("I", "C") == 6);
+    try
+    {
+        t3.findCheapest("A", "Hd");
+    }
+    catch (const logic_error &e)
+    {
+        assert(string(e.what()) != "");
+    }
 
-	t1.addLine(3, {"A", "Z", "M"}).optimize();
+    assert(t3.findCheapest("A", "E") == 1);
+    assert(t3.findCheapest("A", "I") == 3);
+    assert(t3.findCheapest("A", "M") == 4);
+    assert(t3.findCheapest("A", "A") == 0);
+    assert(t3.findCheapest("F", "R") == 5);
+    assert(t3.findCheapest("S", "Z") == 3);
+    assert(t3.findCheapest("S", "T") == 1);
+    assert(t3.findCheapest("A", "V") == 4);
+    assert(t3.findCheapest("A", "W") == 4);
+    assert(t3.findCheapest("A", "Z") == 4);
 
-	assert(t1.findCheapest("A", "M") == 3);
-	assert(t1.findCheapest("C", "K") == 7);
-	assert(t1.findCheapest("B", "L") == 7);
-	assert(t1.findCheapest("I", "C") == 6);
+    CPublicTransport newTransport;
 
-	cout << "ALL ASSERTS PASSED!" << endl;
-	return 0;
+    newTransport.addLine(1, {"Staromestska", "Muzeum", "Mustek", "Namesti Miru", "Jiriho z Podebrad"})
+    .addLine(3, {"Hlavni Nadrazi", "Orionka", "Baterie", "Petriny", "Jiriho z Podebrad"})
+    .addLine(2, {"Kamenicka", "Letenske namesti", "Baterie", "Petriny", "Namesti Miru", "Malovanka"})
+    .addLine(6, {"Vitezne namesti", "I P Pavlova", "Andel"})
+    .addLine(4, {"Pankrac", "Kacerov", "Budejovicka", "Prazskeho povstani", "I P Pavlova", "Mustek", "Staromestska"})
+    .addLine(5, {"Hlavni Nadrazi", "Florenc", "Bila labut", "Namesti Republiky", "Mustek", "Narodni trida"})
+    .addLine(3, {"Muzeum", "Hlavni Nadrazi", "Florenc", "Krizikova", "Invalidovna"})
+    .addLine(7, {"Jiriho z Podebrad", "Florenc", "Namesti Republiky", "Staromestska"})
+    .addLine(8, {"Muzeum", "Nadrazi Holesovice", "Vltavska", "Strossmayerovo namesti", "Letenske namesti"})
+    .addLine(9, {"Hlavni Nadrazi", "Nadrazi Holesovice", "Vltavska", "Florenc", "Bila labut"})
+    .addLine(10, {"Staromestska", "Malostranska", "Hradcanska", "Dejvicka", "Vitezne namesti"})
+    .addLine(2, {"Mustek", "Narodni trida", "Karlovo namesti", "I P Pavlova", "Namesti Miru"})
+    .addLine(4, {"Krizikova", "Karlovo namesti", "Andel", "Radlicka", "Jinonice"})
+    .addLine(3, {"Nadrazi Holesovice", "Kobylisy", "Ladvi", "Strizkov", "Prosek", "Letnany"})
+    .addLine(5, {"Nadrazi Holesovice", "Vltavska", "Florenc", "Hlavni Nadrazi", "Muzeum", "I P Pavlova"})
+    .addLine(6, {"Vitezne namesti", "Dejvicka", "Hradcanska", "Malostranska", "Staromestska"}).optimize();
+
+    assert(newTransport.findCheapest("Staromestska", "Baterie") == 3);
+    assert(newTransport.findCheapest("Staromestska", "Staromestska") == 0);
+    assert(newTransport.findCheapest("Staromestska", "Namesti Miru") == 1);
+    assert(newTransport.findCheapest("Staromestska", "Hlavni Nadrazi") == 4);
+    assert(newTransport.findCheapest("Orionka", "Kamenicka") == 5);
+    assert(newTransport.findCheapest("Vitezne namesti", "Andel") == 6);
+    assert(newTransport.findCheapest("Pankrac", "Staromestska") == 4);
+    assert(newTransport.findCheapest("Hlavni Nadrazi", "Narodni trida") == 5);
+    assert(newTransport.findCheapest("Muzeum", "Invalidovna") == 3);
+    assert(newTransport.findCheapest("Jiriho z Podebrad", "Staromestska") == 1);
+    assert(newTransport.findCheapest("Mustek", "Strossmayerovo namesti") == 9);
+    assert(newTransport.findCheapest("Vitezne namesti", "Bila labut") == 12);
+    assert(newTransport.findCheapest("Hradcanska", "Karlovo namesti") == 9);
+    assert(newTransport.findCheapest("Invalidovna", "Jinonice") == 7);
+    assert(newTransport.findCheapest("Prosek", "Budejovicka") == 12);
+    assert(newTransport.findCheapest("Baterie", "I P Pavlova") == 4);
+    assert(newTransport.findCheapest("Krizikova", "Malostranska") == 10);
+    assert(newTransport.findCheapest("Dejvicka", "Kacerov") == 10);
+    assert(newTransport.findCheapest("Florenc", "Radlicka") == 7);
+    assert(newTransport.findCheapest("Hradcanska", "Narodni trida") == 9);
+    assert(newTransport.findCheapest("Invalidovna", "Letenske namesti") == 6);
+    assert(newTransport.findCheapest("Vltavska", "Malostranska") == 12);
+    assert(newTransport.findCheapest("Andel", "Kobylisy") == 14);
+    assert(newTransport.findCheapest("Bila labut", "Prosek") == 12);
+    assert(newTransport.findCheapest("Radlicka", "Vitezne namesti") == 10);
+    assert(newTransport.findCheapest("Hradcanska", "Jiriho z Podebrad") == 7);
+    assert(newTransport.findCheapest("Karlovo namesti", "Letnany") == 10);
+    assert(newTransport.findCheapest("Letenske namesti", "Strizkov") == 11);
+
+    t3.addLine(1, {"Staromestska", "Muzeum", "Mustek", "Namesti Miru", "Jiriho z Podebrad"})
+    .addLine(3, {"Hlavni Nadrazi", "Orionka", "Baterie", "Petriny", "Jiriho z Podebrad"})
+    .addLine(2, {"Kamenicka", "Letenske namesti", "Baterie", "Petriny", "Namesti Miru", "Malovanka"});
+
+    assert(t3.findCheapest("Staromestska", "Baterie") == 3);
+    assert(t3.findCheapest("Staromestska", "Staromestska") == 0);
+    assert(t3.findCheapest("Staromestska", "Namesti Miru") == 1);
+    assert(t3.findCheapest("Staromestska", "Hlavni Nadrazi") == 4);
+    assert(t3.findCheapest("Orionka", "Kamenicka") == 5);
+
+    t3.addLine(6, {"Vitezne namesti", "I P Pavlova", "Andel"});
+
+    try
+    {
+        t3.findCheapest("Orionka", "Andel");
+    }
+    catch (const logic_error &e)
+    {
+        assert(string(e.what()) != "");
+    }
+
+    cout << "ALL ASSERTS PASSED!" << endl;
+    return 0;
 }
